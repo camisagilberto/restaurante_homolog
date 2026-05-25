1239c41e872adeae0c9d074385a4c1ad7d0f1348
camisagilberto <gibacamisa9@yahoo.com.br>
2026-05-25 15:11:57 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from .db import init_db
 from .routes.admin import admin_bp
 from .routes.client import client_bp
 from .routes.kitchen import kitchen_bp
+from .routes.owner_admin import owner_admin_bp
 from .security import csrf_protect, init_security, inject_globals
 from .utils import format_currency
 
@@ -19,6 +20,7 @@ def create_app(config_object: type[Config] | None = None) -> Flask:
     app.register_blueprint(client_bp)
     app.register_blueprint(admin_bp)
     app.register_blueprint(kitchen_bp)
+    app.register_blueprint(owner_admin_bp)
 
     app.jinja_env.filters['currency'] = format_currency
     app.before_request(csrf_protect)
